a164192fdecab02fa027fd260886203969473a64
gerprog - Gerard Daly <ger_daly@yahoo.com>
2014-03-26 19:50:27 +0000

    added html calculator and css new buttons

---
+++ js/calculator1.js
@@ -0,0 +1,22 @@
+var Calculator = (function() {
+
+    // Private stuff up here
+    var converterValue = 2.2;
+
+
+    // Public methods here
+    return {
+        convertFromPoundsToKIlo: function(weight) {
+            var num = parseInt(weight);
+            if (isNaN(num) == true) {
+                throw new Error ("Not a number");
+            }
+            num = num / converterValue;
+            return Math.round(num);
+        },
+
+        convertFromMetricToImperial: function(weight) {
+            return weight * converterValue;
+        }
+    }
+}());
