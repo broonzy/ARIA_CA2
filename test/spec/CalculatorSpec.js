75d409a198880712ef924348aefe5f9d36013ffc
gerprog - Gerard Daly <ger_daly@yahoo.com>
2014-03-26 19:17:31 +0000

    updated the CalculatorSpec.js

--- test/spec/CalculatorSpec.js
+++ test/spec/CalculatorSpec.js
@@ -1,6 +1,15 @@
-describe("My Converter", function() {
-    it("should convert imperial weight to metric", function() {
-        expect(Converter.convertFromPoundsToKIlo(2.2)).toBe(1);
+describe("My Calculator", function() {
+    it("should add valueA to valueB", function() {
+        expect(Calculator.addValueAtoValueB(2 + 2)).toBe(4);
+    });
+    it("should subtract valueA from valueB", function() {
+        expect(Calculator.subtractValueAfromValueB(2 - 2)).toBe(0);
+    });
+    it("should multiply valueA by valueB", function() {
+        expect(Calculator.multiplyValueAbyValueB(3 * 3)).toBe(9);
+    });
+    it("should divide valueA by valueB", function() {
+        expect(Calculator.divideValueAbyValueB(3 / 3)).toBe(1);
     });
     it("should be able to deal with strings", function(){
         expect(function() {Converter.convertFromPoundsToKIlo("hello")}).toThrow(new Error("Not a number"))
